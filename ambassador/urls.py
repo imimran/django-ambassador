c9d8c7772ae590d261e1f80f5de2f96477f23a9d
imimran <engr.aih@gmail.com>
2021-06-06 16:02:48 +0600

    Setup Redis

--- ambassador/urls.py
+++ ambassador/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-#from .views import 
+from .views import ProductBackAPIView, ProductFontAPIView
 
 urlpatterns = [
+
     path('auth/', include('common.urls')),
+    path('products/front', ProductFontAPIView.as_view()),
+    path('products/back', ProductBackAPIView.as_view() ),
+    
 ]
--- ambassador/views.py
+++ ambassador/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from core.models import Product
+from .serializers import ProductSerializer
+
+class ProductFontAPIView(APIView):
+
+    def get(self, _):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response({'error': False, 'data': serializer.data})
+
+class ProductBackAPIView(APIView):
+
+    def get(self, _):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response({'error': False, 'data': serializer.data})        
 
 
