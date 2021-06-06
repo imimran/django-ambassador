fc0f189fa08921a1f9f188395d89232af3bd8ce0
imimran <engr.aih@gmail.com>
2021-06-06 18:05:06 +0600

    Apply Pagination

--- ambassador/views.py
+++ ambassador/views.py
@@ -6,7 +6,7 @@ from django_redis import get_redis_connection
 from core.models import Product
 from .serializers import ProductSerializer
 from django.core.cache import cache 
-import time
+import time, math
 
 
 class ProductFontAPIView(APIView):
@@ -37,7 +37,29 @@ class ProductBackAPIView(APIView):
                 if (keyword.lower() in p.title.lower()) or (keyword.lower() in p.description.lower())
             })
 
-        serializer = ProductSerializer(products, many=True)
-        return Response({'error': False, 'data': serializer.data})        
+        total = len(products)    
+
+        # sort = request.query_params.get('sort', None)
+        # if sort == 'asc':
+        #     products.sort(key=lambda p: p.price)
+        # elif sort == 'desc':
+        #     products.sort(key=lambda p: p.price, reverse=True)
+
+
+
+        per_page = 2
+        page = int(request.query_params.get('page', 1))
+        start = (page - 1) * per_page
+        end = page * per_page
+
+        data = ProductSerializer(products[start:end], many=True).data
+        return Response({
+            'data': data,
+            'meta': {
+                'total': total,
+                'current_page': page,
+                'total_page': math.ceil(total / per_page)
+            }
+        })      
 
 
